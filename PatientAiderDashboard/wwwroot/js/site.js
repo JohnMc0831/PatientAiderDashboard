bd9c0fa7747d3acf9d947bead9314300b5fee451
John McConnell <johnmc0831@gmail.com>
2018-06-02 22:09:28 -0400

    Settled on HTML5Sortable for sorting topics.

--- PatientAiderDashboard/wwwroot/js/site.js
+++ PatientAiderDashboard/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    sortable('.topicList', {
+        placeholderClass: "foo"
+    });
+
+    $(".saveTopicOrder").on("click", function(e) {
+        e.preventDefault();
+        var currList = $(this).closest('.topicList');
+        var sortedList = $(currList).sortable("widget");
+        var sortedArray = sortedList.toArray();
+    });
+});
