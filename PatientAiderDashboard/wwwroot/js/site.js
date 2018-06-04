2773689f6480109ad0b293d013757dc3503e4e70
John McConnell <johnmc0831@gmail.com>
2018-06-03 22:49:45 -0400

    Sorting and updating of  TopicSortOrder() on Section  is nearly complete.

--- PatientAiderDashboard/wwwroot/js/site.js
+++ PatientAiderDashboard/wwwroot/js/site.js
@@ -9,8 +9,19 @@ $(document).ready(function () {
 
     $(".saveTopicOrder").on("click", function(e) {
         e.preventDefault();
-        var currList = $(this).closest('.topicList');
-        var sortedList = $(currList).sortable("widget");
-        var sortedArray = sortedList.toArray();
+        console.log("inside .saveTopicOrder click event!");
+        var result = sortable($("#" + $(this).data('listid')), 'serialize');
+        console.log('returning serialized topicList for' + $(this).data("listid"));
+
+        //construct string of ids to store in db...
+        var topicOrder = "";
+        $.each(result[0].items, function (index, item) {
+            var $topicItem = $("<div>" + item.html.trim() + "</div>");
+            var topicId = $topicItem.find('a').data('id');
+            topicOrder += topicId + ",";
+        });
+        topicOrder = topicOrder.substring(0, topicOrder.length - 1);
+        //TODO:  ajax call to persist new order to db.
+        alert("New Topic Order: " + topicOrder + "");
     });
 });
